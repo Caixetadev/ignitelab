6c3786c0634a810b8228b35128e5dbdaf50a16ce
caixeta <caixetadev@gmail.com>
2022-06-29 21:52:54 -0300

    feat: import Video and Sidebar in Event component

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,13 @@
-import { Header } from "../components";
+import { Header, Sidebar, Video } from "../components";
 
 export function Event() {
-  return <Header />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
+  );
 }
